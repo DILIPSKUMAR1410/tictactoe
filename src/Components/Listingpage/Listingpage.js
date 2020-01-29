d6fb85e682b46c3ed0d6820a1507f9ea18cf4f6c
adarshsuresh07 <adarshsuresh@cet.ac.in>
2020-01-29 23:12:15 +0530

    Final

--- src/Components/Listingpage/Listingpage.js
+++ src/Components/Listingpage/Listingpage.js
@@ -1,30 +1,41 @@
 import React from 'react'
 import './Listingpage.css'
-class Listingpage extends React.Component{
-    constructor(props){
+class Listingpage extends React.Component {
+    constructor(props) {
         super(props);
-        this.state={
-            computer:0,
-            you:0
+        this.state = {
+            computer: 0,
+            you: 0
         };
+        this.gohome = this.gohome.bind(this);
+
     }
-    componentDidMount(){
-        if(localStorage.getItem("tictactoe")){
-            this.setState({computer:JSON.parse(localStorage.getItem("tictactoe"))[0]});
-            this.setState({you:JSON.parse(localStorage.getItem("tictactoe"))[1]});
+    componentDidMount() {
+        if (localStorage.getItem("tictactoe")) {
+            this.setState({ computer: JSON.parse(localStorage.getItem("tictactoe"))[0] });
+            this.setState({ you: JSON.parse(localStorage.getItem("tictactoe"))[1] });
         }
     }
-    render(){
-        return(
+    gohome(){
+        this.props.history.push("/");
+    }
+    render() {
+        return (
             <div className="list">
+                <div className="header">
+                    <img src={require("../../Assets/tictactoe.png")} onClick={this.gohome}/>
+                    tictactoe
+                </div>
+                <div className="scoreboard">
                 <div className="table">
                     <div className="tabledata">
-                    <div className="comp">Computer</div>
-                    <div className="tr">{this.state.computer}</div>
+                        <div className="comp">Computer</div>
+                        <div className="tr">{this.state.computer}</div>
                     </div>
                     <div className="tabledata">
-                    <div className="you">You</div>  
-                    <div className="tr">{this.state.you}</div>
+                        <div className="you">You</div>
+                        <div className="tr">{this.state.you}</div>
+                    </div>
                     </div>
                 </div>
             </div>
